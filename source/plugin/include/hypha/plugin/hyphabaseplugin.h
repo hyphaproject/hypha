dbce83bb37ad17d04cd107c40097d541d82c0091
Christian <chriamue@gmail.com>
2016-05-21 22:58:37 +0200

    definition for UNUSED parameters

--- source/plugin/include/hypha/plugin/hyphabaseplugin.h
+++ source/plugin/include/hypha/plugin/hyphabaseplugin.h
@@ -14,6 +14,15 @@
 #endif
 #include <hypha/plugin/plugin_api.h>
 
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#endif
+
 namespace hypha {
 namespace plugin {
 
