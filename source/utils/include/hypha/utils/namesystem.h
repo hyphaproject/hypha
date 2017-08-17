9bf44db3ec2a8135f758eaac9b2fdbf8af5f4de9
Christian <chriamue@gmail.com>
2017-08-17 08:28:40 +0200

    added namesystem class

---
+++ source/utils/include/hypha/utils/namesystem.h
@@ -0,0 +1,66 @@
+// Copyright (c) 2017 Hypha
+#pragma once
+
+#include <hypha/utils/utils_api.h>
+#include <map>
+#include <string>
+
+#include <Poco/Runnable.h>
+#include <Poco/Thread.h>
+
+namespace hypha {
+namespace utils {
+
+/**
+ * @brief The NameSystem class
+ * Runs kadnode in a thread.
+ * Provides ip lookup by hostname or public key fron kadnode.
+ */
+class UTILS_API NameSystem : public Poco::Runnable {
+ public:
+  NameSystem();
+
+  /**
+   * @brief run
+   * threads run function, used for kadnode
+   */
+  void run() override;
+
+  /**
+   * @brief start
+   * starts namesystem thread
+   */
+  void start();
+
+  /**
+   * @brief stop
+   * stops namesystem thread
+   */
+  void stop();
+
+  /**
+   * @brief hostnameToIP
+   * gets ip by hostname
+   * @param hostname
+   * @return ip address as string
+   */
+  std::string hostnameToIP(std::string hostname);
+
+  /**
+   * @brief publicKeyToIP
+   * gets ip by public key from dht
+   * @param publicKey
+   * @return ip address as string
+   */
+  std::string publicKeyToIP(std::string publicKey);
+
+ protected:
+  // Kadnode kadnode;
+
+  std::map<std::string, std::string> ipCache;
+
+  bool running = false;
+  Poco::Thread thread;
+};
+}
+}
---
+++ source/utils/source/namesystem.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2017 Hypha
+
+#include <hypha/utils/namesystem.h>
+
+#include <Poco/Net/DNS.h>
+
+using namespace hypha::utils;
+
+NameSystem::NameSystem() {
+  std::string privateKey = "***********";
+  std::string publicKey = "examplehashstring";
+  std::string port;
+  std::string announce = publicKey + ".hypha:" + port;
+  // kadnode_set("announce", publicKey.c_str());
+  // kadnode_set("port", 6881);
+  // kadnode_set("query-tld", ".hypha");
+  // kadnode_set("load-key-string", privateKey.c_str());
+  // kadnode_init();
+}
+
+void NameSystem::run() {
+  while (running) {
+    // kadnode_run_loop();
+  }
+}
+
+void NameSystem::start() {
+  running = true;
+  thread.setName("namesystem");
+  thread.start(*this);
+}
+
+void NameSystem::stop() { running = false; }
+
+std::string NameSystem::hostnameToIP(std::string hostname) {
+  if (ipCache.find(hostname) == ipCache.end()) {
+    Poco::Net::IPAddress address = Poco::Net::DNS::resolveOne(hostname);
+    ipCache.insert(
+        std::pair<std::string, std::string>(hostname, address.toString()));
+    return address.toString();
+  }
+  return ipCache[hostname];
+}
+
+std::string NameSystem::publicKeyToIP(std::string publicKey) {
+  if (ipCache.find(publicKey) == ipCache.end()) {
+    // IP addrs[1];
+    // kadnode_lookup( publicKey.c_str(), addrs, 1 );
+    // ...
+  }
+  return ipCache[publicKey];
+}
